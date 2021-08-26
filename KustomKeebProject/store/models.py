93b5e8e4701ffd7ac5ae49497676c2b61f23bf82
Jordan-Edwards <jordanedwards441@gmail.com>
2021-08-25 20:18:44 -0500

    Registered Category & Product for Super User

--- KustomKeebProject/store/models.py
+++ KustomKeebProject/store/models.py
@@ -1,14 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=220, db_index=True),
-    slug = models.SlugField(max_length=220, unique=True),
+    name = models.CharField(max_length=255, db_index=True, default='Place Holder')
+    slug = models.SlugField(max_length=255, unique=True, default='Place Holder')
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -32,6 +35,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
-
-    # def get_absolute_url(self):
-    #     return reverse('store:category_list', args=[self.slug])
